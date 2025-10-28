ae05dd3ac51ae8e3daf7f106a1810c6d43bc66f3
bitar011 <victorbitarr@gmail.com>
2025-10-28 19:27:33 -0300

    práticando operações com linguagem C montando um algoritmo que verifica média de dois números

--- Algoritmo&ProgramaçãoEstruturada/Unidade1/aula1.c
+++ Algoritmo&ProgramaçãoEstruturada/Unidade1/aula1.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello World!");
+    float num1, num2, media;
+    printf("Digite o primeiro numero: ");
+    scanf("%f", &num1);
+    
+    printf("Digite o segundo numero: ");
+    scanf("%f", &num2);
+
+    media = (num1 + num2)/2;
+    printf("Media = %.2f", media);
 
     return 0;
 }
