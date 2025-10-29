f262ee416dd31e4e34448de7d55e9667e4031239
bitar011 <victorbitarr@gmail.com>
2025-10-29 11:34:26 -0300

    Adição de explicação da lógica do que foi feito para prática

--- Algoritmo&ProgramaçãoEstruturada/Unidade1/aula2.c
+++ Algoritmo&ProgramaçãoEstruturada/Unidade1/aula2.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 
+//Definição de constantes
 #define taxaDeImposto 0.1
 #define descontoPadrão 0.05
 
 int main()
 {
+    //Definição de variáveis
     float valor, valorDesconto, valorImposto, valor_final;
+    //Saída com comando do que deve ser feito e entrada do usuário
     printf("Digite o valor de venda do produto: ");
     scanf("%f", &valor);
 
+    //Processamento que resultará no valor final
     valorImposto = valor * taxaDeImposto;
     valorDesconto = valor * descontoPadrão;
     valor_final = valor + valorImposto - valorDesconto;
 
+    //Saída com valor final resultado do processamento
     printf("O valor final e: R$%2.f", valor_final);
     return 0;
 }
