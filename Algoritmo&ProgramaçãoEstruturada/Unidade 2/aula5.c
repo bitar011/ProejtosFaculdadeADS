76be6876b45a03c5966013179d54b1676d9b55df
bitar011 <victorbitarr@gmail.com>
2025-11-07 18:42:31 -0300

    Prática da quinta e ultima aula da segunda unidade, revisando conceitos, utilizando C

---
+++ Algoritmo&ProgramaçãoEstruturada/Unidade 2/aula5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    int primeiro = 0, segundo = 1, proximo;
+
+    printf("Digite o numero de termos da sequencia de Fibonacci que voce deseja calcular: ");
+    scanf("%d", &n);
+
+    printf("Sequencia de Fibonacci ate o termo %d:\n", n);
+
+    for (int i = 0; i < n; i++)
+    {
+        if(i<= 1){
+            proximo = i;
+        } else {
+            proximo = primeiro + segundo;
+            primeiro = segundo;
+            segundo = proximo;
+        }
+        printf("%d", proximo);
+    }
+
+    return 0;
+}
