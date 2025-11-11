ac1d501ef9358d6f85c2a1261c462db3ddf3b5fc
bitar011 <victorbitarr@gmail.com>
2025-11-11 19:10:25 -0300

    Prática de vetores da primeira aula da terceira unidade, utilizando C.

---
+++ Algoritmo&ProgramaçãoEstruturada/Unidade 3/aula1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(){
+
+    char cpf1[15]; //Vetor CPF formatado com pontos e traço
+    char cpf2[12] = ""; //Vetor CPF sem formatação, inicializado como vazio
+
+    int i = 0, n = 0;
+
+    printf("Digite seu CPF na forma NNN.NNN.NNN-NN: \n");
+    scanf("%s", cpf1);
+
+    //Laço para percorrer todo o vetor cpf1
+    //Remover pontos e traços e armazenar apenas os dígitos
+    for (i = 0; i < 14; i++) {
+        if (cpf1[i] == '.' || cpf1[i] == '-') {
+            continue; //Ignora pontos e traços
+        } else {
+            //Armazena apenas os dígitos em cpf2
+            cpf2[n] = cpf1[i];
+
+            n++; //controla o tamanho do cpf2
+        }
+    }
+
+    //Adiciona o terminador nulo no final da string
+    cpf2[n] = '\0';
+
+    // Imprime o CPF formatado sem pontos e traços
+    printf("\n\nCPF formatado = %s", cpf2);
+
+    return 0;
+}
