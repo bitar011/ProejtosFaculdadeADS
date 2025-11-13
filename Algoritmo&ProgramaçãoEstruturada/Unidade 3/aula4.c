c54af98a2c661ff0537231351f56f25c71a7c958
bitar011 <victorbitarr@gmail.com>
2025-11-13 20:29:21 -0300

    Pequena prática sobre ponteiros, para algoritmos e programação estruturada, utilizando C.

---
+++ Algoritmo&ProgramaçãoEstruturada/Unidade 3/aula4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+    int vetor[5] = {1, 2, 3, 4, 5};
+    int *ponteiro = vetor; //Associando o ponteiro ao vetor
+
+    for (int i = 0; i < 5; i++) {
+
+        *(ponteiro + i) += 10;
+    }
+
+    printf("Elementos do vetor apos adicionar 10:\n");
+
+    for (int i = 0; i < 5; i++) {
+
+        printf("%d ", vetor[i]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
