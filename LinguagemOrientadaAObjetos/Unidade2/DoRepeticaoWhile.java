ce0793e280eb55be78674200e33491e25e3d6059
bitar011 <victorbitarr@gmail.com>
2026-03-12 10:35:54 -0300

    Prática de estruturas de repetição com java, da terceira aula, prática com cada estrutura demonstrada

---
+++ LinguagemOrientadaAObjetos/Unidade2/DoRepeticaoWhile.java
@@ -0,0 +1,12 @@
+public class DoRepeticaoWhile {
+    public static void main(String[] args) {
+        int i = 1;
+        
+        do{ 
+            System.out.println(i);
+            i++;
+        }while (i <= 10);
+
+        System.out.println("Fim do Programa!");
+    }
+}
