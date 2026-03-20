cb1996f6bd3e5dbf79fa44650685a5bb00668a7e
bitar011 <victorbitarr@gmail.com>
2026-03-19 21:00:44 -0300

    Prática de classes abstratas com java, prática da terceira aula da Unidade 3

---
+++ LinguagemOrientadaAObjetos/Unidade3/Aula 3/Aluno.java
@@ -0,0 +1,16 @@
+
+class Aluno extends Pessoa {
+
+    private int matricula;
+
+    public Aluno(String nome, int matricula) {
+        super(nome);
+        this.matricula = matricula;
+    }
+
+    @Override
+    public void apresentar() {
+        System.out.println("Eu sou o aluno: " + nome + " e minha matricula é " + matricula);
+    }
+
+}
---
+++ LinguagemOrientadaAObjetos/Unidade3/Aula 3/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Aluno aluno1 = new Aluno("Victor", 11);
+        aluno1.apresentar();
+
+        System.out.println("Hello World");
+    }
+}
